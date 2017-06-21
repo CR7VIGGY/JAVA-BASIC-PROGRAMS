e396e65e57cb2277f7099e670be0e0cdcd09a346
VIGHNESH TIWARI <vighnesh.tiwari@yahoo.com>
2017-06-21 20:02:30 +0530

    Create Addition of two no.s

---
+++ Addition of two no.s
@@ -0,0 +1,17 @@
+//Vighnesh Tiwari Codes //CR7VIGGY GITHUB PROFILE................AIT PUNE,INDIA
+import java.util.Scanner;
+public class cal
+{
+	public static void main(String args[])
+	{
+		Scanner x = new Scanner(System.in);
+		double a,b,c;
+		System.out.println("enter first no.");
+		a = x.nextDouble();
+		System.out.println("enter second no.");
+		b = x.nextDouble();
+		c = a + b;
+		System.out.println(c);
+	}
+}
+		
